65fc6ca0fe088c9323a680b7741b5fd8044bdaaf
Juan Guillermo Buitrago Calle <64021772+Juanbc2@users.noreply.github.com>
2023-10-21 01:25:33 -0500

    login structure done

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,52 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:once_upon_a_time/components/textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: const Text('Once Upon A Time'),
+          backgroundColor: Colors.white,
+          title: const Text('Once Uppon A Time',
+              style: TextStyle(color: Colors.black)),
         ),
-        body: SafeArea(
-            child: Column(
-          children: [
-            const SizedBox(
-              height: 50,
-            ),
-            Icon(
-              Icons.lock,
-              size: 100,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Text('Welcome to Once Upon A Time',
-                style: TextStyle(fontSize: 20, color: Colors.grey[700])),
-            SizedBox(height: 50),
-            TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                  hintText: 'Enter valid username'),
-            ),
-            SizedBox(height: 20),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                  hintText: 'Enter secure password'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to second route when tapped.
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        )));
+        body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SafeArea(
+                child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  Text(
+                    'Welcome Back',
+                    style: TextStyle(fontSize: 35, color: Colors.purple[700]),
+                    textAlign: TextAlign.left,
+                  ),
+                ]),
+                const SizedBox(height: 20),
+                Text(
+                    'Fill out the information in order to access your account.',
+                    style: TextStyle(fontSize: 20, color: Colors.grey[700])),
+                const SizedBox(height: 50),
+                MyTextField(
+                    controller: usernameController,
+                    hintText: 'Email',
+                    obscureText: false),
+                const SizedBox(height: 20),
+                MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ))),
+                    onPressed: () {
+                      // Navigate to second route when tapped.
+                    },
+                    child: const Text('Go'),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text('Or sign up with',
+                    style: TextStyle(fontSize: 16, color: Colors.grey[700])),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.white),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.grey.shade400),
+                        ))),
+                    onPressed: () {
+                      // Navigate to second route when tapped.
+                    },
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.g_mobiledata, color: Colors.black),
+                        SizedBox(width: 10),
+                        Text('Continue with Google',
+                            style: TextStyle(color: Colors.black)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ))));
   }
 }
